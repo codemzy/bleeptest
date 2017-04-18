0dc36c6e4049e0bc3a3fd3b12763af31159e10b7
Emma J <codemzy@gmail.com>
2017-04-18 16:01:30 +0000

    Add width to bar

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@
 
 $(document).ready(function() {
    
-   $('h1').text('Level 1');
-    
+   $('#start').on('click', function() {
+       $('h1').text('Level 1');
+       $('#bar').removeClass('indeterminate');
+       $('#bar').width('50%').addClass('determinate');
+   });
 });
