c72a1d27fd7edc70ad87c698ee1f41175860fa95
Emma J <codemzy@gmail.com>
2017-04-18 16:09:12 +0000

    Added button class change on start

--- js/index.js
+++ js/index.js
@@ -2,10 +2,19 @@
 
 /* global $ */
 
+var data20 = {
+    1: { runs: 7, time: 9.00, speed: 8 },
+    2: { runs: 8, time: 8.00, speed: 9 }
+};
+
 $(document).ready(function() {
+    
+    var level = 1;
    
    $('#start').on('click', function() {
-       $('h1').text('Level 1');
+       $('h1').text('Level ' + level);
+       $('#start').addClass('disabled');
+       $('#stop').removeClass('disabled');
        $('#bar').removeClass('indeterminate');
        $('#bar').width('50%').addClass('determinate');
    });
